1d1ac4df6d4edb6fb44532344c53392dd7273a54
yujiahuo <yujiahuo7@gmail.com>
2024-02-20 16:41:11 -0600

    Home page updates

--- scripts/setupPage.js
+++ scripts/setupPage.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
-	$("#header").load("components/header.html");
+	if ($("#header").length){
+		$("#header").load("components/header.html");
+		$(document).on( "click", function(event) {
+			switch (event.target.id) {
+				case "trainingServicesMenuItem":
+					expandTrainingServices();
+					break;
+				case "professionalServicesMenuItem":
+					expandProfessionalServices();
+					break;
+				default:
+					collapseDropdowns();
+			}
+		  } );
+	}
 	$("#footer").load("components/footer.html");
 	//If home page, hide logo
 });
+
+function expandTrainingServices() {
+	document.getElementById("professionalServicesMenuItem").classList.remove("selected");
+	document.getElementById("trainingServicesMenuItem").classList.add("selected");
+}
+
+function expandProfessionalServices() {
+	document.getElementById("trainingServicesMenuItem").classList.remove("selected");
+	document.getElementById("professionalServicesMenuItem").classList.add("selected");
+}
+
+function collapseDropdowns() {
+	document.getElementById("trainingServicesMenuItem").classList.remove("selected");
+	document.getElementById("professionalServicesMenuItem").classList.remove("selected");
+}
