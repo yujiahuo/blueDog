26e1c6f1e241a7adf7c433c8f240621a5e2576c3
yujiahuo <yujiahuo7@gmail.com>
2024-04-13 20:26:11 -0500

    mostly styles

--- scripts/setupPage.js
+++ scripts/setupPage.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
 		});
 	}
 	$("#footer").load("components/footer.html");
-	//If home page, hide logo
+
+	observeForAnimation();
 });
 
 function toggleMenuItem(menuItemId) {
@@ -30,4 +31,24 @@ function collapseDropdowns() {
 
 function toggleMobileMenu() {
 	$("#menu").toggleClass("mobileHidden");
+}
+
+function observeForAnimation() {
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+
+			if (entry.isIntersecting) {
+				entry.target.classList.add('appear');
+				return; // if we added the class, exit the function
+		 	}
+	  
+		// We're not intersecting, so remove the class!
+			entry.target.classList.remove('appear');
+		});
+	});
+	
+	const animatedEls = document.querySelectorAll(".appearOnScroll");
+	animatedEls.forEach((el) => {
+		observer.observe(el);
+	});
 }
