6a67fcc479bc7b02eb21d5dcd822fe73c927202a
yujiahuo <yujiahuo7@gmail.com>
2024-07-19 15:29:56 -0500

    Fixes to product and home

--- components/call-button.js
+++ components/call-button.js
@@ -6,7 +6,7 @@ class CallButton extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
 			<a href="tel:+16082135304" class="button">
-				Call today to schedule an appointment<br/><b>608-213-5304</b>
+				Start with a free 30 min call: <br/><b>608-213-5304</b>
 			</a>
 		`
 	}
