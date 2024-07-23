818b19832ddb86b4175716375d7d4fc2bd26062a
yujiahuo <yujiahuo7@gmail.com>
2024-07-23 16:09:03 -0500

    Clean up old stuff. Seminar updates

--- old_src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/footer.css';
-
-export default function Footer() {
-	return (
-		<div id="footer">
-			Copyright 2024 - Yujia was here
-		</div>
-	);
-}
--- old_src/components/logo.tsx
+++
@@ -1,9 +0,0 @@
-import logoImg from '../images/blueDogLogo-white.png';
-
-export default function Logo() {
-	return (
-		<a href="/" className="logo">
-			<img className="logoImg" src={logoImg} />
-		</a>
-	);
-}
